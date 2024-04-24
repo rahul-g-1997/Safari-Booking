d59475c89e0e65b549b9ec14684c3f84093b5181
Rahul Gaydhane <rahulgaydhane7@gmail.com>
2024-04-24 14:15:05 +0530

    add  : data

--- src/rtk/store.js
+++ src/rtk/store.js
@@ -1,11 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import loginReducer from "./reducer/loginReducer";
-import userReducer from "./reducer/userReducer";
+import authReducer from "./reducer/userReducer";
 import otpReducer from "./reducer/otpReducer";
 const store = configureStore({
   reducer: {
+    auth: authReducer, // Combine reducers, 'authReducer' is responsible for managing authentication state
     login: loginReducer,
-    user: userReducer,
     otp: otpReducer,
   },
 });
