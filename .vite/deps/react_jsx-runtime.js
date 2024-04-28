73534f800e3a8bc57010e4bea206075b02cab1bc
Rahul Gaydhane <rahulgaydhane7@gmail.com>
2024-04-28 16:20:27 +0530

    remove extra

--- .vite/deps/react_jsx-runtime.js
+++
@@ -1,7 +0,0 @@
-import {
-  require_jsx_runtime
-} from "./chunk-O55OJW5D.js";
-import "./chunk-HAZNF34R.js";
-import "./chunk-WXXH56N5.js";
-export default require_jsx_runtime();
-//# sourceMappingURL=react_jsx-runtime.js.map
